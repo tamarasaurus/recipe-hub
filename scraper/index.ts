d416eff53c9d61ef179a8308730addc932a607fd
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-10 21:45:52 +0200

    Scrape links and contents

--- scraper/index.ts
+++ scraper/index.ts
@@ -3,17 +3,31 @@ import csv from './attributes/csv';
 import ingredientList from './attributes/ingredient-list';
 import duration from './attributes/duration';
 
-const sources = {
+import * as moment from 'moment';
+import * as flatten from 'array-flatten';
+
+const weeks = [
+  moment().add(1, 'week').startOf('isoWeek').format('YYYY-MM-DD'),
+  moment().add(2, 'week').startOf('isoWeek').format('YYYY-MM-DD'),
+  moment().add(3, 'week').startOf('isoWeek').format('YYYY-MM-DD')
+]
+
+
+const contracts = {
   quitoque: {
-    links: [
-      'https://www.quitoque.fr/recettes/poelee-de-saumon-aux-epices-nordiques/2019-20'
-    ],
-    contract: require('./contracts/quitoque.json')
+    links: require('./contracts/quitoque-link.json'),
+    recipes: require('./contracts/quitoque.json'),
+    urls: [
+      `https://www.quitoque.fr/au-menu/2_personnes/${weeks[0]}`,
+      `https://www.quitoque.fr/au-menu/2_personnes/${weeks[1]}`,
+      `https://www.quitoque.fr/au-menu/2_personnes/${weeks[2]}`,
+    ]
   }
 }
 
-function collectPagesToScrape() {
-
+function collectLinks(url: string, contract) {
+  const scraper = new Scraper(url, contract);
+  return scraper.scrapePage();
 }
 
 function scrapePage(url: string, contract) {
@@ -22,10 +36,25 @@ function scrapePage(url: string, contract) {
     'ingredient-list': ingredientList,
     'duration': duration,
   })
+  console.log(url);
+  // scraper.scrapePage().then(items => {
+  //   console.log('scraped items', items)
+  // })
+}
 
-  scraper.scrapePage().then(items => {
-    console.log('scraped items', items)
+async function scrapeRecipes() {
+  const linkPromises = contracts.quitoque.urls.map((url: string) => {
+    return collectLinks(url, contracts.quitoque.links)
+  });
+
+  const links = await Promise.all(linkPromises).then((...values) => {
+    return flatten(values).map((url: any) => url.link).filter((url: string) => url !== null);
+  })
+
+  // Run in job queue
+  const itemPromises = links.map((link: string) => {
+    return scrapePage(link, contracts.quitoque.recipes);
   })
 }
 
-scrapePage(sources.quitoque.links[0], sources.quitoque.contract)
+scrapeRecipes();
