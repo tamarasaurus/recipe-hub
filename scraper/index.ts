d67f9f44f857394ed37ec2cd9fbefabccca11d6e
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-10 21:52:57 +0200

    Use job queue

--- scraper/index.ts
+++ scraper/index.ts
@@ -2,16 +2,39 @@ import Scraper from 'contract-scraper';
 import csv from './attributes/csv';
 import ingredientList from './attributes/ingredient-list';
 import duration from './attributes/duration';
-
 import * as moment from 'moment';
 import * as flatten from 'array-flatten';
+import * as Queue from 'bull';
+
+const scrapingQueue = new Queue('scraping', 'redis://127.0.0.1:6379');
+
+scrapingQueue.process((job: any, done) => {
+
+  const scraper = new Scraper(job.url, job.contract, {
+    'csv': csv,
+    'ingredient-list': ingredientList,
+    'duration': duration,
+  })
+
+  console.log(job.url);
+
+  scraper.scrapePage().then(items => {
+    console.log('scraped items', items)
+    done(null, items)
+  }).catch((e: Error) => {
+    done(e, [])
+  });
+});
+
+scrapingQueue.on('completed', (job: any, result) => {
+  console.log(job.url, result)
+})
 
 const weeks = [
   moment().add(1, 'week').startOf('isoWeek').format('YYYY-MM-DD'),
   moment().add(2, 'week').startOf('isoWeek').format('YYYY-MM-DD'),
   moment().add(3, 'week').startOf('isoWeek').format('YYYY-MM-DD')
 ]
-
 
 const contracts = {
   quitoque: {
@@ -30,18 +53,6 @@ function collectLinks(url: string, contract) {
   return scraper.scrapePage();
 }
 
-function scrapePage(url: string, contract) {
-  const scraper = new Scraper(url, contract, {
-    'csv': csv,
-    'ingredient-list': ingredientList,
-    'duration': duration,
-  })
-  console.log(url);
-  // scraper.scrapePage().then(items => {
-  //   console.log('scraped items', items)
-  // })
-}
-
 async function scrapeRecipes() {
   const linkPromises = contracts.quitoque.urls.map((url: string) => {
     return collectLinks(url, contracts.quitoque.links)
@@ -51,10 +62,9 @@ async function scrapeRecipes() {
     return flatten(values).map((url: any) => url.link).filter((url: string) => url !== null);
   })
 
-  // Run in job queue
-  const itemPromises = links.map((link: string) => {
-    return scrapePage(link, contracts.quitoque.recipes);
-  })
+  links.forEach((url: string) => scrapingQueue.add({
+    url, contract: contracts.quitoque.recipes
+  }))
 }
 
 scrapeRecipes();
