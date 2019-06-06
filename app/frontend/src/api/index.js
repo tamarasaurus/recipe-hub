071755e07822cfd3f8c21bef041d27887b9060d9
Tamara Robichet <tamara@robichet.com>
2019-06-06 23:29:27 +0200

    Fix API url

--- app/frontend/src/api/index.js
+++ app/frontend/src/api/index.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://192.168.1.44:8000/api'
+const API_URL = 'http://localhost:8000/api'
 
 const fetchApi = async (query, options) => {
   const res = await fetch(API_URL + query, options)
