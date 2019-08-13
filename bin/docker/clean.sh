162ac6d4cc9db9f8efe7549b30d4002ad650b6ae
Tamara Robichet <tamara.robichet@akeneo.com>
2019-08-13 16:55:53 +0200

    Add comments

--- bin/docker/clean.sh
+++ bin/docker/clean.sh
@@ -2,5 +2,4 @@
 
 docker-compose exec postgres psql -U postgres postgres -c '\pset pager off' -c "drop table recipe cascade;drop table auth_user cascade;drop table auth_user_recipe cascade;"
 docker-compose exec redis redis-cli FLUSHALL
-docker-compose restart app
-docker-compose exec app npm run setup
+docker-compose restart api app
